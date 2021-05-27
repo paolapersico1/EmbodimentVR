cfc185f89b7a47b041686dd84b8c7311f58f68fe
Paola <p.ably@hotmail.it>
2021-05-27 13:11:17 +0200

    turning fixed

--- Embodiment/Assets/Scripts/VRRig.cs
+++ Embodiment/Assets/Scripts/VRRig.cs
@@ -55,8 +55,8 @@ public class VRRig : MonoBehaviour
         {
             transform.position = headConstraint.position + headBodyOffset;
             //transform.forward = Vector3.ProjectOnPlane(head.rigTarget.transform.up, Vector3.up).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized,
-                                            Time.deltaTime * turnSmoothness);
+            transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized,
+                                            Time.deltaTime * turnSmoothness); 
 
             head.Map(vrHead);
             leftHand.Map(vrLeftHand);
