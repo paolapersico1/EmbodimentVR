9483955c8ff68c81856f49a80ca0a13d2f4063af
Paola <32850381+emilypeek1@users.noreply.github.com>
2021-06-18 15:18:45 +0200

    fix

---
+++ Embodiment/Assets/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    static WebCamTexture backCam;
+
+    void Start()
+    {
+	WebCamDevice[] devices = WebCamTexture.devices;
+	foreach (var device in devices){
+		Debug.Log(device.name);
+	}
+	
+        if (backCam == null)
+            backCam = new WebCamTexture(devices[0].name);
+
+        GetComponent<Renderer>().material.mainTexture = backCam;
+
+        if (!backCam.isPlaying)
+            backCam.Play();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
