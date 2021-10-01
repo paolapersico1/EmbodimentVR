a710f8c6cff402a184c536cf1a3c94567ba5eea1
Paola <p.ably@hotmail.it>
2021-10-01 20:07:56 +0200

    new avatar added

---
+++ Embodiment/Assets/Scripts/Calibrator.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[System.Serializable]
+public class Calibrator : MonoBehaviour
+{
+    public bool vrSimulator = false;
+
+    public float playerHeadHeight;
+    public float playerArmLength;
+
+    public Transform vrHead;
+
+    public float hmdHeadRootOffset = 0.03f;  //hmd usually 5cm above head root
+    public float handWristOffset = 0.18f;  //hand length
+
+    public void SetPlayerParams()
+    {
+        if (vrSimulator)
+        {
+            playerHeadHeight = 1.5f;
+            playerArmLength = 0.75f;
+        }
+        else
+        {
+            playerHeadHeight = vrHead.position.y - hmdHeadRootOffset;
+            playerArmLength = (vrHead.position.y / 2.0f) - handWristOffset;
+        }
+        
+    }
+
+    public float GetPlayerHeadHeight()
+    {
+        return playerHeadHeight;
+    }
+
+    public float GetPlayerArmLength()
+    {
+        return playerArmLength;
+    }
+}
--- Embodiment/Assets/Scripts/VRRig.cs
+++ Embodiment/Assets/Scripts/VRRig.cs
@@ -8,77 +8,137 @@ public class VRMap
 {
     public Transform vrTarget;
     public Transform rigTarget;
+    
     public Vector3 trackingPositionOffset;
     public Vector3 trackingRotationOffset;
 
     public void Map(Transform vrTransform)
     {
         vrTarget = vrTransform;
-        //TransformPoint -> Transforms trackingPositionOffset from local space to world space
         rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
-        //Euler -> Returns a rotation that rotates z degrees around the z axis, etc.
         rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
     }
 
 }
 public class VRRig : MonoBehaviour
 {
-    public float turnSmoothness;
+    public float avatarHeadHeight;
+    public float avatarArmLength;
+    //public Vector3 avatarEyeHeadOffset;
+    public float torsoOffset;
+    
     public VRMap head;
     public VRMap leftHand;
     public VRMap rightHand;
 
-    private Transform vrHead;   //camera
-    private Transform vrLeftHand;   //left controller
-    private Transform vrRightHand;  //right controller
-  
-    public Vector3 headBodyOffset;
+    private Transform vrHead;
+    private Transform vrLeftHand;
+    private Transform vrRightHand;
+
     public bool bodyRotation = true;
-    public int rotThreshold = 10;
+    public int rotThreshold;
+    public float turnSmoothness;
+    public float handsTorsoRotation; //DEBUG
+    public float crouchingThreshold;
+
+    private Animator animator;
+    private float playerArmLength;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
 
         XRRig rig = FindObjectOfType<XRRig>();
         vrHead = rig.transform.Find("Camera Offset/Main Camera");
         vrLeftHand = rig.transform.Find("Camera Offset/LeftHand Controller");
         vrRightHand = rig.transform.Find("Camera Offset/RightHand Controller");
+    }
 
-        //difference in position between the avatar head and the avatar body
-        headBodyOffset = transform.position - head.rigTarget.position;
+    public void Calibrate()
+    {
+        GameObject camOffset = GameObject.Find("XR Rig/Camera Offset");
+
+        float playerHeadHeight = camOffset.GetComponent<Calibrator>().GetPlayerHeadHeight();
+        playerArmLength = camOffset.GetComponent<Calibrator>().GetPlayerArmLength();
+
+        avatarHeadHeight = head.rigTarget.position.y - transform.position.y;
+        camOffset.transform.position = new Vector3(0, avatarHeadHeight - playerHeadHeight, 0);
+
+        //how longer are the avatar's arms
+        avatarArmLength = Vector3.Distance(rightHand.rigTarget.position, head.rigTarget.position);
+        float armOffset = avatarArmLength - playerArmLength;
+        leftHand.trackingPositionOffset = new Vector3(0, 0, armOffset);
+        rightHand.trackingPositionOffset = new Vector3(0, 0, armOffset);
+
+        //Transform cam = camOffset.transform.Find("Main Camera/Camera");
+        //cam.localPosition = avatarEyeHeadOffset;
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (vrRightHand)
+        {
+
+            Vector3 goalPosition = vrRightHand.TransformPoint(rightHand.trackingPositionOffset);
+            Quaternion goalRotation = vrRightHand.rotation * Quaternion.Euler(rightHand.trackingRotationOffset);
+
+            float reach = animator.GetFloat("RightHand");
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, reach);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, goalPosition);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, reach);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, goalRotation);
+        }
+        if (vrLeftHand)
+        {
+            Vector3 goalPosition = vrLeftHand.TransformPoint(leftHand.trackingPositionOffset);
+            Quaternion goalRotation = vrLeftHand.rotation * Quaternion.Euler(leftHand.trackingRotationOffset);
+
+            float reach = animator.GetFloat("LeftHand");
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, reach);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, goalPosition);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, reach);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, goalRotation);
+        }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void OnAnimatorMove()
     {
-        transform.position = head.rigTarget.position + headBodyOffset;
+        Vector3 handsPosition = Vector3.Lerp(rightHand.rigTarget.position, leftHand.rigTarget.position, 0.5f);
+        Vector3 newTorsoPosition = head.rigTarget.position - new Vector3(0, avatarHeadHeight, torsoOffset);
 
+        transform.position = new Vector3(newTorsoPosition.x, 
+                                        IsCrouched(head.rigTarget.position)? newTorsoPosition.y : transform.position.y, 
+                                        newTorsoPosition.z);
 
-        //if the angle between the head rotation on the y axis and the body rotation on the y axis is greater than 90 degrees
-        //eulerAngles represents the rotation in world space
-        float headTorsoRotation = RotationDifference(transform, head.rigTarget);
-        float leftHandTorsoRotation = RotationDifference(transform, leftHand.rigTarget);
-        float rightHandTorsoRotation = RotationDifference(transform, rightHand.rigTarget);
-        if (bodyRotation &&
-            Mathf.Abs(headTorsoRotation - leftHandTorsoRotation) < rotThreshold &&
-            Mathf.Abs(headTorsoRotation - rightHandTorsoRotation) < rotThreshold)
+        handsTorsoRotation = Vector3.Angle(
+                                Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized,
+                                Vector3.ProjectOnPlane(handsPosition - transform.position, Vector3.up).normalized
+                                );
+
+        if (bodyRotation && handsTorsoRotation > rotThreshold)
         {
-            //rotate also the body by projecting the head z axis on the y axis 
-            //linear interpolation with the previous position is used to smooth the movement
+            //transform.forward = Vector3.ProjectOnPlane(head.rigTarget.transform.up, Vector3.up).normalized;
             transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(head.rigTarget.forward, Vector3.up).normalized,
                                         Time.deltaTime * turnSmoothness);
         }
 
-        //map the real to the virtual
         head.Map(vrHead);
-        leftHand.Map(vrLeftHand);
-        rightHand.Map(vrRightHand);
     }
 
-    private float RotationDifference(Transform firstTransform, Transform secondTransform)
+    private bool IsCrouched(Vector3 headPosition)
     {
-        return Quaternion.Angle(Quaternion.Euler(0, firstTransform.rotation.eulerAngles.y, 0),
-                Quaternion.Euler(0, secondTransform.rotation.eulerAngles.y, 0));
+        RaycastHit hit;
+        Ray ray = new Ray(headPosition, Vector3.down);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            float distance = Vector3.Distance(headPosition, hit.transform.position);
+            if (distance < (avatarHeadHeight * crouchingThreshold))
+                return false;
+        }
+
+        return true;
     }
 }
