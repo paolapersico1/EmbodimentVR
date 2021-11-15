dc28aa0ac6707e8f58c7ed14dc1709ea0fa630f6
Igor <igor_iurevici@hotmail.it>
2021-11-15 16:19:51 +0100

    gender raid control added

--- Embodiment/Assets/Scripts/AIController.cs
+++ Embodiment/Assets/Scripts/AIController.cs
@@ -23,6 +23,8 @@ public class AIController : MonoBehaviour
     private float timeToReturn = 120;
     private bool alive = true;
     private bool destToSet = true;
+    private bool isFemale = false;
+    private bool noRaid = false;
 
 
     // Start is called before the first frame update
@@ -38,7 +40,6 @@ public class AIController : MonoBehaviour
         wp2 = GameObject.FindGameObjectsWithTag("wp2");
         wp3 = GameObject.FindGameObjectsWithTag("wp3");
         agent = this.GetComponent<NavMeshAgent>();
-        agentObj = this.GetComponent<GameObject>();
         agent.SetDestination(wp1[Random.Range(0, wp1.Length)].transform.position);
 
         // Animator controller
@@ -53,8 +54,8 @@ public class AIController : MonoBehaviour
 
         // Head Controller
         timeToLook = Random.Range(timeToLook, timeToLook + 30);
-        this.GetComponent<HeadController>().lookObj = GameObject.Find("XR Rig/Player Offset/Camera Offset/Main Camera/Camera").transform;
-        //this.GetComponent<HeadController>().lookObj = GameObject.Find("CameraTest").transform;
+        //this.GetComponent<HeadController>().lookObj = GameObject.Find("XR Rig/Player Offset/Camera Offset/Main Camera/Camera").transform;
+        this.GetComponent<HeadController>().lookObj = GameObject.Find("CameraTest").transform;
         this.GetComponent<HeadController>().enabled = false;
         this.GetComponent<HeadController>().ikActive = true;
         Debug.Log("enabled: " + this.GetComponent<HeadController>().enabled);
@@ -69,15 +70,22 @@ public class AIController : MonoBehaviour
 
         // WP3 timer
         timeToReturn = Random.Range(timeToReturn, timeToReturn + 50);
+
+        // Getting prefab gender
+        if (gameObject.name.Contains("female")) {
+            isFemale = true;
+        }
+        
+        int r = Random.Range(0,9);
+
+        // if female: 20% prob to raid, 80% to just walk
+        // if male: 80% prob to raid, 20% to just walk
+        if ((r > 1 && isFemale) || (r < 2 && !isFemale)) {
+            noRaid = true;
+        }
     }
 
-   // void SetTimeOffset(float offset)
-   // {
-   //     timeToLook += offset;
-   //     timeToTalk += offset;
-   //     timeToRaid += offset;
-   //     timeToReturn += offset;
-   // }
+
 
     void Update()
     {
@@ -89,62 +97,63 @@ public class AIController : MonoBehaviour
                 agent.SetDestination(wp1[Random.Range(0, wp1.Length)].transform.position);
             }
 
-            // Start looking
-            if(timeToLook > 0)
-            {
-                timeToLook -= Time.deltaTime;
-            }
-            else {
-                this.GetComponent<HeadController>().enabled = true;
-            }
-            
-            // Phase2: wp1 to wp2
-            if(timeToRaid > 0)
-            {
-                timeToRaid -= Time.deltaTime;
-            }
-            else if (phase != 3)
-            {
-                phase = 2;
-
-                // agent.SetDestination(wp2[Random.Range(0, wp2.Length)].transform.position);
-                if (destToSet) {
-                    agent.SetDestination(wp2[Random.Range(0, wp2.Length)].transform.position);
-                    destToSet = false;
+            if (!noRaid) {
+                // Start looking
+                if(timeToLook > 0)
+                {
+                    timeToLook -= Time.deltaTime;
                 }
-                if(agent.remainingDistance < 1) { // || ((agent.velocity.magnitude/agent.speed) < 0.2 && (agent.remainingDistance < 2))) {
-                    animator.SetBool("isIdle", true);
-                    animator.SetBool("isWalking", false);
-                    agent.isStopped = true;
+                else {
+                    this.GetComponent<HeadController>().enabled = true;
                 }
-            }
+                
+                // Phase2: wp1 to wp2
+                if(timeToRaid > 0)
+                {
+                    timeToRaid -= Time.deltaTime;
+                }
+                else if (phase != 3)
+                {
+                    phase = 2;
 
-            // Talk timer
-            if(timeToTalk > 0)
-            {
-                timeToTalk -= Time.deltaTime;
-            }
-            else {
-                animator.SetBool("isTalking", true);
-            }
+                    // agent.SetDestination(wp2[Random.Range(0, wp2.Length)].transform.position);
+                    if (destToSet) {
+                        agent.SetDestination(wp2[Random.Range(0, wp2.Length)].transform.position);
+                        destToSet = false;
+                    }
+                    if(agent.remainingDistance < 1) { // || ((agent.velocity.magnitude/agent.speed) < 0.2 && (agent.remainingDistance < 2))) {
+                        animator.SetBool("isIdle", true);
+                        animator.SetBool("isWalking", false);
+                        agent.isStopped = true;
+                    }
+                }
 
-            // Phase3: wp2 to wp3
-            if(timeToReturn > 0)
-            {
-                timeToReturn -= Time.deltaTime;
-            }
-            else {
-                phase = 3;
-                agent.SetDestination(wp3[0].transform.position);
-                animator.SetBool("isIdle", false);
-                animator.SetBool("isWalking", true);
-                agent.isStopped = false;
-               // if (agent.remainingDistance < 1) {
-               //     Destroy(agent);
-               //     //Destroy(agentObj);
-               //     alive = false; // the agent is dead here
-               // }
+                // Talk timer
+                if(timeToTalk > 0)
+                {
+                    timeToTalk -= Time.deltaTime;
+                }
+                else {
+                    animator.SetBool("isTalking", true);
+                }
+
+                // Phase3: wp2 to wp3
+                if(timeToReturn > 0)
+                {
+                    timeToReturn -= Time.deltaTime;
+                }
+                else {
+                    phase = 3;
+                    agent.SetDestination(wp3[0].transform.position);
+                    animator.SetBool("isIdle", false);
+                    animator.SetBool("isWalking", true);
+                    agent.isStopped = false;
+                   // if (agent.remainingDistance < 1) {
+                   //     Destroy(gameObject);
+                   //     alive = false; // the agent is dead here
+                   // }
+                }
             }
         }
-    }
+    }   
 }
